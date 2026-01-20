513a0bb78b80351870ff11551590e2196b3c1ace
Bogdan Ratiu <bogdan.ratiu@velocitaconsultancy.uk>
2026-01-20 16:58:54 +0100

    My first commit ever

---
+++ session1.py
@@ -0,0 +1,25 @@
+# This is my first code
+# I am just learning python
+# print('Hello World')
+print("Hello World")
+print("7+6")
+print(7+6)
+# print('And John asked: "How are you?"')
+print("hello"" ""world")
+# print('"'"'")
+
+print("3 + 4 =", 3+4, "and 5 x 6 =", 5*6)
+print("I have $", 100+200, ".")
+print("I have $", 100+200, ".", sep="!!")
+# end is used to tell print what to do after it prints
+print("1", end="")
+print("2", end="")
+print("3", end="!!\n\n")
+print("4", end="\n\n\n")
+print("5")
+print("\"'")
+print(2+3*6/2)
+print(4/2)
+print(4//2)
+
+
